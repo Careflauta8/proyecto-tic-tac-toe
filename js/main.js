8a142c38f21a6457f9fa2af09cf3746e5629524c
Diego Sánchez <diegosanchezlondono@gmail.com>
2022-12-31 18:51:59 +0100

    se agrego funcion ganador y CPU

--- js/main.js
+++ js/main.js
@@ -2,15 +2,30 @@
 let turno= 1;
 let jugadas = [];
 let modo = 'jvj';
-
+let ultimaSelect;
 
 buttonpulsado = (id) => {
-   	if (modo === 'jvj') {
-		juegaHumano(id);
-	} else {
-		juegaHumano(id);
-		juegaCPU();
-	} 
+	if (!hayGanador()) {
+		if (modo === 'jvj') {
+			juegaHumano(id);
+		} else {
+			juegaHumano(id);
+			if (turno <= 6 ) {
+				juegaCPU();
+			} else if (turno > 6  && jugadas.length === 6 && 
+				((turno % 2 == 1 && modo === 'cvj' ) 
+				|| (turno % 2 == 0 && modo === 'jvc'))) 
+			{
+				juegaCPU();
+				juegaCPU();
+			}
+		}
+	}
+	
+	if (hayGanador()) {
+		document.getElementById('ganador').innerHTML = turno % 2 ? 'Ganador X' : 'Ganador O';
+	}
+	document.getElementById('turno').innerHTML = turno % 2 ? 'Turno X' : 'Turno O';
 }
 
 juegaHumano = (id) => {
@@ -30,48 +45,84 @@ juegaHumano = (id) => {
 			turno = turno + 1;
 		}
 	}
-	console.log('---------------');
 }
 
 juegaCPU = () => {
 	let posiblesJugadas = calcularPosibleJugadas();
-	console.log('posiblesJugadas: ', posiblesJugadas);
-
 	let id = posiblesJugadas[ Math.floor(Math.random() * posiblesJugadas.length)];
-	console.log('id: ', id);
 	juegaHumano(id);
+	ultimaSelect = id;
 }
 
 calcularPosibleJugadas = () => {
-	let posibleJugadas = [1,2,3,4,5,6,7,8,9];
+	let posibleJugadas = [];
+
 	if (turno <= 6 ) {
-		posibleJugadas = posibleJugadas.filter(pos => jugadas.find(jugada => jugada.id !== pos));
+		if (jugadas.length > 0) {
+			[1,2,3,4,5,6,7,8,9].forEach(posi => {
+				let juga = jugadas.find(jugada => posi === jugada.id);
+				if (!juga) {
+					posibleJugadas.push(posi);
+				}
+			})
+		} else {
+			return [1,2,3,4,5,6,7,8,9]
+		}
+		
 	} else {
-		if (jugadas.length === 6) { 
-			posibleJugadas = [];
+		if (jugadas.length === 6) {
 			jugadas.forEach(jugada => {
 				if (jugada.jugador === turno % 2) {
 					posibleJugadas.push(jugada.id);
 				}
 			});
 		} else {
-			posibleJugadas = posibleJugadas.filter(pos => jugadas.find(jugada => jugada.id !== pos));
+			// ultimaSelect
+			[1,2,3,4,5,6,7,8,9].forEach(posi => {
+				let juga = jugadas.find(jugada => posi === jugada.id);
+				if (!juga && ultimaSelect != posi ) {
+					posibleJugadas.push(posi);
+				}
+			})
 		}
 	}
 
 	return posibleJugadas;
 }
 
+hayGanador = () => {
+	let result = false;
+	for (let jugadaGanadora of ganador) {
+		let jugada1 = jugadas.find(jugada => jugadaGanadora[0] == jugada.id);
+		let jugada2 = jugadas.find(jugada => jugadaGanadora[1] == jugada.id);
+		let jugada3 = jugadas.find(jugada => jugadaGanadora[2] == jugada.id);
+		console.log('jugadaGanadora :', jugadaGanadora);
+		console.log('jugada1 :', jugada1);
+		console.log('jugada2 :', jugada2);
+		console.log('jugada3 :', jugada3);
+		console.log('---------\n');
+		if (jugada1 && jugada2 && jugada3) {
+			result = jugada1.jugador == jugada2.jugador && jugada3.jugador == jugada1.jugador
+		}
+		
+		if (result) {
+			break;
+		}	
+	}
+	console.log('result: ', result);
+	return result;
+}
 
 
 reset = () => {
 	turno = 1;
 	jugadas = [];
-	if ( modo === 'cvj') {
-		juegaCPU();
-	}
 	for (let i = 1; i <= 9; i++) {
 		document.querySelector(`.casilla${i}`).style.background = 'white'
+	}
+
+	if ( modo === 'cvj') {
+		juegaCPU();
 	}
 }
 
@@ -87,18 +138,15 @@ cvj = () => {
 	modo = 'cvj';
 }
 
-
-
-
 const ganador = [
-	[0, 1, 2], 
-	[3, 4, 5], 
-	[6, 7, 8], 
-	[0, 3, 6], 
+	[1, 2, 3], 
+	[4, 5, 6], 
+	[7, 8, 9], 
 	[1, 4, 7], 
 	[2, 5, 8], 
-	[0, 4, 8], 
-	[2, 4, 6]
+	[3, 6, 9], 
+	[1, 5, 9], 
+	[3, 5, 7]
 ];
 
 
